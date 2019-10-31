55ce148be23b27f8a88bb64521e9e4a1138edcce
Jeremy Marshall <jeremystuartmarshall@gmail.com>
2019-11-01 06:35:06 +1100

    rebase

--- install-kb.sh
+++ install-kb.sh
@@ -7,7 +7,7 @@ if [[ ! -d /usr/local/kubebuilder ]];then
 	arch=$(go env GOARCH)
 
 	# download kubebuilder and extract it to tmp
-	curl -sL https://go.kubebuilder.io/dl/2.0.1/${os}/${arch} | tar -xz -C /tmp/
+	curl -sL https://go.kubebuilder.io/dl/2.0.0/${os}/${arch} | tar -xz -C /tmp/
 
 	# move to a long-term location and put it on your path
 	# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
