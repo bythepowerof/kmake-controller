062f905a88f15a74f335f8bd9a09b7a0d3a785b4
Jeremy Marshall <jeremystuartmarshall@gmail.com>
2019-11-04 07:21:29 +1100

    travis cache kb in tmp

--- install-kb.sh
+++ install-kb.sh
@@ -5,11 +5,18 @@ set -e
 if [[ ! -d /usr/local/kubebuilder ]];then
 	os=$(go env GOOS)
 	arch=$(go env GOARCH)
+	version=2.0.1
 
-	# download kubebuilder and extract it to tmp
-	curl -sL https://go.kubebuilder.io/dl/2.0.1/${os}/${arch} | tar -xz -C /tmp/
+	if [[ ! -f /tmp/kubebuilder/kubebuilder_${version}_${os}_${arch} ]];then
+
+		# created by travis cache after first run
+		mkdir -p /tmp/kubebuilder
+
+		# download kubebuilder and extract it to tmp
+		curl -sL https://go.kubebuilder.io/dl/${version}/${os}/${arch} | tar -xz -C /tmp/kubebuilder
+	fi
 
 	# move to a long-term location and put it on your path
 	# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-	sudo mv /tmp/kubebuilder_2.0.1_${os}_${arch} /usr/local/kubebuilder
+	sudo cp -r /tmp/kubebuilder/kubebuilder_${version}_${os}_${arch} /usr/local/kubebuilder
 fi
