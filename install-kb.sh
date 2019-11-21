3249967617286cb93df97d7ce3e3b04ee7084ec1
Jeremy Marshall <jeremystuartmarshall@gmail.com>
2019-11-21 21:34:15 +1100

    fix travis

--- install-kb.sh
+++ install-kb.sh
@@ -7,16 +7,16 @@ if [[ ! -d /usr/local/kubebuilder ]];then
 	arch=$(go env GOARCH)
 	version=2.2.0
 
-	if [[ ! -f /tmp/kubebuilder/kubebuilder_${version}_${os}_${arch} ]];then
+	if [[ ! -f ~/tmp/kubebuilder/kubebuilder_${version}_${os}_${arch} ]];then
 
 		# created by travis cache after first run
-		mkdir -p /tmp/kubebuilder
+		mkdir -p ~/tmp/kubebuilder
 
 		# download kubebuilder and extract it to tmp
-		curl -sL https://go.kubebuilder.io/dl/${version}/${os}/${arch} | tar -xz -C /tmp/kubebuilder
+		curl -sL https://go.kubebuilder.io/dl/${version}/${os}/${arch} | tar -xz -C ~/tmp/kubebuilder
 	fi
 
 	# move to a long-term location and put it on your path
 	# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-	sudo cp -r /tmp/kubebuilder/kubebuilder_${version}_${os}_${arch} /usr/local/kubebuilder
+	sudo cp -r ~/tmp/kubebuilder/kubebuilder_${version}_${os}_${arch} /usr/local/kubebuilder
 fi
